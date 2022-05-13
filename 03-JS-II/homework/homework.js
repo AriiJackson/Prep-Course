e4c49d41cbb52c6e21d630dcc6a32605959ab434
Arii Jackson <ariijackson@hotmail.com>
2022-05-13 14:41:22 -0300

    Correccion de algunos ejercicios en la 02-JS-I y terminacion de los ejercicios en la 03-JS-II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -140,6 +140,14 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+
+  if (numero == Math.floor(numero)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+
 }
 
 function fizzBuzz(numero) {
@@ -170,6 +178,24 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return 'Hay negativos';
+  }
+  else if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return 'Error';
+  }
+  else if (num1 > num2 && num1 > num3 && num1 > 0) {
+    return 'Número 1 es mayor y positivo';
+  }
+  else if (num3 > num2 || num3 > num1) {
+    num3++;
+    return num3;
+  }
+  else {
+    return false;
+  }
+
 }
 
 function esPrimo(numero) {
@@ -212,13 +238,26 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
 
-  
+  var tabla = [];
+
+  for (var i = 0; i <= 10; i++) {
+    tabla.push(i * 6);
+  }
+
+  return tabla; 
 
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  
+  if (numero > 99 && numero < 1000) {
+    return true;
+  }
+  else {
+    return false;
+  }
   
 }
 
